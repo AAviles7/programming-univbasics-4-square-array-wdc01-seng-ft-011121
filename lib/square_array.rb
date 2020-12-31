c78dcdd94f7594c220b40244007954518834b049
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 02:00:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_numbers = Array.new
   array.length.times do
-    temp = (array.shift)^2
+    temp = array.shift
     new_numbers.push(temp)
   end
   return new_numbers
