4a97df003a42cd40cc6e3a7dd293279f5ed3b3ac
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 02:00:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_numbers = Array.new
   array.length.times do
     temp = array.shift
-    temp = temp*2
+    temp = temp**2
     new_numbers.push(temp)
   end
   return new_numbers
